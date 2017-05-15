5079b19d66b968c222e20ba850fa3662dd2485cc
prasetyaws <thekingfps@gmail.com>
2017-05-15 13:31:53 +0700

    Final

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl526/movieinsight/Popular.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl526/movieinsight/Popular.java
@@ -1,21 +1,14 @@
 package id.sch.smktelkom_mlg.privateassignment.xirpl526.movieinsight;
 
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -46,148 +39,34 @@ import id.sch.smktelkom_mlg.privateassignment.xirpl526.movieinsight.Service.API;
 import id.sch.smktelkom_mlg.privateassignment.xirpl526.movieinsight.Service.GsonGetRequest;
 import id.sch.smktelkom_mlg.privateassignment.xirpl526.movieinsight.Service.VolleyController;
 
-public class MainActivity extends AppCompatActivity {
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Popular extends Fragment {
 
     GridView gv;
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
-        gv = (GridView) findViewById(R.id.movieitem_list);
+    public Popular() {
+        // Required empty public constructor
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            if (position == 0) {
-                return new TopRated();
-            } else if (position == 1) {
-                return new Popular();
-            } else if (position == 2) {
-                return new Upcoming();
-            } else {
-                return PlaceholderFragment.newInstance(position + 1);
-            }
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Top Rated";
-                case 1:
-                    return "Popular";
-                case 2:
-                    return "Upcoming";
-            }
-            return null;
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_popular, container, false);
+        Popular.FetchMovies fetchMovies = new Popular.FetchMovies();
+        String url = "http://api.themoviedb.org/3/movie/popular";
+        fetchMovies.execute(url);
+        gv = (GridView) view.findViewById(R.id.movie_list);
+        return view;
     }
 
     public class FetchMovies extends AsyncTask<String, Void, List<Movie>> {
-        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-        private final String LOG_TAG = FetchMovies.class.getSimpleName();
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        private final String LOG_TAG = Popular.FetchMovies.class.getSimpleName();
 
         @Override
         protected void onPreExecute() {
@@ -215,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                             Log.e("FLOW", "onErrorResponse: ", error);
                         }
                     });
-            VolleyController.getInstance(this).addToRequestQueue(myRequest);
+            VolleyController.getInstance(getActivity()).addToRequestQueue(myRequest);
 
             String sortingCriteria = params[0];
             Uri builtUri = Uri.parse(API.API_URL).buildUpon()
@@ -359,14 +238,14 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(final List<Movie> movies) {
             super.onPostExecute(movies);
             progressDialog.hide();
-            GridViewAdapter gridViewAdapter = new GridViewAdapter(getApplicationContext(), movies);
+            GridViewAdapter gridViewAdapter = new GridViewAdapter(getActivity().getApplicationContext(), movies);
             gv.setAdapter(gridViewAdapter);
 
             gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Movie movie = movies.get(position);
-                    Intent intent = new Intent(MainActivity.this, MovieDetail.class);
+                    Intent intent = new Intent(getActivity(), MovieDetail.class);
                     intent.putExtra("poster_path", movie.getPoster_path());
                     intent.putExtra("backdrop_path", movie.getBackdrop_path());
                     intent.putExtra("year", movie.getRelease_date());
@@ -381,4 +260,5 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
+
 }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl526/movieinsight/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl526.movieinsight.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "21d22e0ffa464aaefeec2e66b681f55c";
+}
